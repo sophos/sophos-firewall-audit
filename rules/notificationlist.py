dffb638029a24459d7768524a03a6bcf7854bc39
Matt Mullen <matt.mullen@sophos.com>
2023-11-13 17:48:48 -0500

    fix: html color only on value

--- rules/notificationlist.py
+++ rules/notificationlist.py
@@ -49,7 +49,7 @@ def eval_notification_list(fw_obj: SophosFirewall,
     for setting in expected_settings.keys():
         expected_list.append(f"{setting}: {expected_settings[setting]}")
         if not expected_settings[setting] == actual_settings[setting]:
-            actual_list.append(f'<a style="color: red">{setting}: {actual_settings[setting]}</a>')
+            actual_list.append(f'{setting}: <a style="color: red">{actual_settings[setting]}</a>')
             result_dict["notification_list"]["status"] = "AUDIT_FAIL"
             result_dict["audit_result"] = "FAIL"
             result_dict["fail_ct"] += 1
