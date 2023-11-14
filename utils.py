5b66f19f6394a0df6e169746d5e76cd46ce83297
Matt Mullen <matt.mullen@sophos.com>
2023-11-14 10:59:16 -0500

    fix: strip AUDIT_ from test status

--- utils.py
+++ utils.py
@@ -16,6 +16,6 @@ def html_status(audit_status):
         audit_status (str): AUDIT_PASS or AUDIT_FAIL
     """
     if audit_status == "AUDIT_PASS":
-        return f'<a style="color: lime; font-weight: bold;">{audit_status}</a>'
+        return f'<a style="color: lime; font-weight: bold;">{audit_status.strip("AUDIT_")}</a>'
     if audit_status == "AUDIT_FAIL":
-        return f'<a style="color: red; font-weight: bold;">{audit_status}</a>'
+        return f'<a style="color: red; font-weight: bold;">{audit_status.strip("AUDIT_")}</a>'
