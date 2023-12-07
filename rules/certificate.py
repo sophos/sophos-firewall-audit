500b8fb618fca47d02482bbf0cd7719c6044b2fa
Matt Mullen <matt.mullen@sophos.com>
2023-12-07 09:47:02 -0500

    fix: certificate audit failure counters

--- rules/certificate.py
+++ rules/certificate.py
@@ -53,7 +53,6 @@ def eval_certificate(fw_obj: SophosFirewall,
         if not expected_settings[setting] == actual_settings[setting]:
             actual_list.append(f"{setting}: {html_red(actual_settings[setting])}")
             result_dict["certificate"]["status"] = "AUDIT_FAIL"
-            result_dict["fail_ct"] += 1
             result_dict["audit_result"] = "FAIL"
             # print(f"expected_settings: {setting}: {expected_settings[setting]}")
             # print(f"actual_settings: {setting}: {actual_settings[setting]}")
@@ -61,6 +60,8 @@ def eval_certificate(fw_obj: SophosFirewall,
             actual_list.append(f"{setting}: {actual_settings[setting]}")
     if result_dict["audit_result"] == "PASS":
         result_dict["pass_ct"] += 1
+    else:
+        result_dict["fail_ct"] += 1
     output = []
 
     output.append([
