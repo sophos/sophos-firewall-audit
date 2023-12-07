dd696747db193f02f0436c588ccf4cc78c289f7e
Matt Mullen <matt.mullen@sophos.com>
2023-12-07 09:49:36 -0500

    fix: backup audit counters

--- rules/backup.py
+++ rules/backup.py
@@ -55,7 +55,6 @@ def eval_backup(fw_obj: SophosFirewall,
         if not str(expected_settings[setting]).lower() == str(actual_settings[setting]).lower():
             actual_list.append(f"{setting}: {html_red(actual_settings[setting])}")
             result_dict["backup"]["status"] = "AUDIT_FAIL"
-            result_dict["fail_ct"] += 1
             result_dict["audit_result"] = "FAIL"
             # print(f"expected_settings: {setting}: {expected_settings[setting]}")
             # print(f"actual_settings: {setting}: {actual_settings[setting]}")
@@ -63,6 +62,8 @@ def eval_backup(fw_obj: SophosFirewall,
             actual_list.append(f"{setting}: {actual_settings[setting]}")
     if result_dict["audit_result"] == "PASS":
         result_dict["pass_ct"] += 1
+    else:
+        result_dict["fail_ct"] += 1
      
     output = []
 
