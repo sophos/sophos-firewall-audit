64863a740e3fb874964d627ea860407c6d6b657b
Matt Mullen <matt.mullen@sophos.com>
2025-05-08 21:48:22 -0400

    fix docker build context

--- terraform/run_pipeline.sh
+++ terraform/run_pipeline.sh
@@ -86,7 +86,7 @@ aws eks update-kubeconfig --region eu-west-1 --name SophosFactory
 export REVISION=$(helm list --filter 'fwaudit' --output=json | jq -r '.[].revision')
 export TAG=$(python -c "import os; print(int(os.environ['REVISION']) + 1)")
 aws ecr get-login-password | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com
-docker build -f ../docker/Dockerfile . -t $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/fwaudit-results:$TAG
+docker build -f ../docker/Dockerfile ../docker -t $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/fwaudit-results:$TAG
 docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/fwaudit-results:$TAG
 
 # Deploy with Helm
