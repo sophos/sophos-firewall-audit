65b60fb6d0583d57ee16db8429700349c5c027fa
Matt Mullen <matt.mullen@sophos.com>
2025-05-09 20:43:26 -0400

    update path to env-vars.json

--- terraform/run_pipeline.sh
+++ terraform/run_pipeline.sh
@@ -28,13 +28,13 @@ mv results_html_web docker/
 # Write SSL cert and key
 # printf "%b" "$SSL_CERT" > ../docker/server.crt
 # printf "%b" "$SSL_KEY" > ../docker/server.key
-jq -r '.SSL_CERT' env0.env-vars.json | \
+jq -r '.SSL_CERT' ./terraform/env0.env-vars.json | \
 awk 'BEGIN {print "-----BEGIN CERTIFICATE-----"} 
      NR==1 {gsub(/-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----/, "")}
      {gsub(/ /, ""); for (i = 1; i <= length($0); i += 64) print substr($0, i, 64)} 
      END {print "-----END CERTIFICATE-----"}' > ./docker/server.crt
 
-jq -r '.SSL_KEY' env0.env-vars.json | \
+jq -r '.SSL_KEY' ./terraform/env0.env-vars.json | \
 awk 'BEGIN {print "-----BEGIN PRIVATE KEY-----"} 
      NR==1 {gsub(/-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----/, "")}
      {gsub(/ /, ""); for (i = 1; i <= length($0); i += 64) print substr($0, i, 64)} 
@@ -46,21 +46,21 @@ mkdir -p ~/.docker
 export DOCKER_HOST='tcp://10.183.4.122:2375'
 export DOCKER_TLS_VERIFY=1
 # printf "%b" "$DOCKER_CA_CERT" > ~/.docker/ca.pem
-jq -r '.DOCKER_CA_CERT' env0.env-vars.json | \
+jq -r '.DOCKER_CA_CERT' ./terraform/env0.env-vars.json | \
 awk 'BEGIN {print "-----BEGIN CERTIFICATE-----"} 
      NR==1 {gsub(/-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----/, "")}
      {gsub(/ /, ""); for (i = 1; i <= length($0); i += 64) print substr($0, i, 64)} 
      END {print "-----END CERTIFICATE-----"}' > ~/.docker/ca.pem
 
 # printf "%b" "$DOCKER_CLIENT_CERT" > ~/.docker/cert.pem
-jq -r '.DOCKER_CLIENT_CERT' env0.env-vars.json | \
+jq -r '.DOCKER_CLIENT_CERT' ./terraform/env0.env-vars.json | \
 awk 'BEGIN {print "-----BEGIN CERTIFICATE-----"} 
      NR==1 {gsub(/-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----/, "")}
      {gsub(/ /, ""); for (i = 1; i <= length($0); i += 64) print substr($0, i, 64)} 
      END {print "-----END CERTIFICATE-----"}' > ~/.docker/cert.pem
 
 # printf "%b" "$DOCKER_CLIENT_KEY" > ~/.docker/key.pem
-jq -r '.DOCKER_CLIENT_KEY' env0.env-vars.json | \
+jq -r '.DOCKER_CLIENT_KEY' ./terraform/env0.env-vars.json | \
 awk 'BEGIN {print "-----BEGIN RSA PRIVATE KEY-----"} 
      NR==1 {gsub(/-----BEGIN RSA PRIVATE KEY-----|-----END RSA PRIVATE KEY-----/, "")}
      {gsub(/ /, ""); for (i = 1; i <= length($0); i += 64) print substr($0, i, 64)} 
